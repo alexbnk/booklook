15ea35403592a13a34e6766bd0c5424ef1deb92d
alex <alexbnk@gmail.com>
2017-02-27 20:30:56 +0200

    latest , i dont remember what i did lol

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
 var isbn;
 var book;
 var apikey = "AIzaSyDFFrq30NUVTot6ubglCDwCC4aHx3PN2Bg" ;
+var isisbn;
+
 
 
 $('.search').on('click', function(){
  isbn = ($('#isbn_search').val());
- if (isbn.length > 4) {
   fetch(isbn);
-
-
- }
 
 });
 
-var postelm=function(bt, bst ,bd,ba,bc) {
+var postelm=function(bid, bt, bst ,bi, bd,ba,bc) {
 
     source = $('#book-template').html();
     template = Handlebars.compile(source);
     newHTML = template(
       {
+        bookid: bid,
         booktitle:bt,
         booksubtitle: bst,
+        bookisbn: bi,
         bookdescription:bd,
         bookauthor:ba,
         bookcover:bc
+
       });
 
   return newHTML;
 }
 
-var showBook = function(obj) {
-//  console.log("showBook:"+obj);
+
+
+
+
+var showBook = function(isThisisbn) {
+console.log('lookup by isbn: '+isThisisbn);
   $('.book').remove();
-for (var i=0; i<9; i++){
+var y;
+
+if (isThisisbn)  {y=1} else {y=10};
+
+for (var i=0; i<y; i++){
+//if (book.items[i].volumeInfo.imageLinks == 'undefined' ){ cover = "."} else {cover = book.items[i].volumeInfo.imageLinks.thumbnail }
+//if (book.items[i].volumeInfo.industryIdentifiers[0] == 'undefined') {identifier = "n/a"} else {identifier = book.items[i].volumeInfo.industryIdentifiers[0].identifier }
+
   $('.result').append(
-   postelm(book.items[i].volumeInfo.title, book.items[i].volumeInfo.subtitle,book.items[i].volumeInfo.description,book.items[i].volumeInfo.authors,book.items[i].volumeInfo.imageLinks.thumbnail  ));
-}
-  }
+
+   postelm(
+            i,
+            book.items[i].volumeInfo.title,
+            book.items[i].volumeInfo.subtitle,
+            book.items[i].volumeInfo.industryIdentifiers[0].identifier,
+            book.items[i].volumeInfo.description,
+            book.items[i].volumeInfo.authors,
+            book.items[i].volumeInfo.imageLinks.thumbnail
+          ));
+
+
+            }
+              $('.book').on('click', function() {
+
+                  isbn = $(this).data('id');
+                  alert(isbn);
+                fetch(isbn);
+                console.log("Book # "+isbn+" clicked");
+              });
+
+            }
+
 
 
 
@@ -44,12 +76,14 @@ for (var i=0; i<9; i++){
 
 
 var fetch = function (query) {
+  alert(query);
+   isisbn = (!isNaN(query) && (query.length == 10 || query.length==13));
 
-  if (isNaN(query)) {
-    console.log(query.length);
-    var getISBN = "https://www.googleapis.com/books/v1/volumes?key="+apikey+"&q="+ query;
+    if (isisbn) {
+      var getISBN = "https://www.googleapis.com/books/v1/volumes?key="+apikey+"&q=isbn:"+ query;
+
   } else {
-    var getISBN = "https://www.googleapis.com/books/v1/volumes?key="+apikey+"&q=isbn:"+ query;
+    var getISBN = "https://www.googleapis.com/books/v1/volumes?key="+apikey+"&q=intitle:"+ query;
    }
 
   console.log("url : "+getISBN);
@@ -61,7 +95,7 @@ var fetch = function (query) {
     success: function(data) {
       console.log('success')
       book = (data);
-      showBook(book);
+      showBook(isisbn);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
